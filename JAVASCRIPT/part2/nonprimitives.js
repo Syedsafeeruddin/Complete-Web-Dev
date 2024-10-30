69afa37cdc2a2631a6dfc989a7c09caca8910144
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2024-10-30 14:16:30 +0500

    JAVASCRIPT PART 4 & 5

---
+++ JAVASCRIPT/part2/nonprimitives.js
@@ -0,0 +1,20 @@
+let username = {
+    firstname: "Syed Safeer",
+    lastname: "Uddin",
+    email: "safeer@gmailcom",
+    "Male or Female": "Male"
+}
+
+console.log(username.email);
+console.log(username['Male or Female']);
+console.log(username);
+console.log(typeof username);
+
+// Array
+let names = ['safeer', 'ali', 'hassan']
+
+console.log(names[2]);
+
+
+
+
---
+++ JAVASCRIPT/part2/primitive.js
@@ -0,0 +1,19 @@
+let balance = 232
+let newBalance = new Number(323)
+
+// console.log(typeof(balance));
+// console.log(typeof(newBalance));
+
+
+// null and undefined
+let fname = null
+let lname = undefined
+
+console.log(fname);
+console.log(lname);
+
+
+// String
+
+let name = 'Safeer'
+console.log(`Hello ${name}`);

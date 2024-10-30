6c8d3ecbef19ea5b127f9ba1c7a9f2fe196ce8bd
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2024-10-30 14:41:55 +0500

    JAVASCRIPT PART 6

---
+++ JAVASCRIPT/part3/conditions.js
@@ -0,0 +1,50 @@
+// Greater Number
+
+// let num1 = 237894623
+// let num2 = 238645234
+
+// if(num1 > num2){
+//     console.log(`num1 is greater`)
+// }
+// else if (num2 > num1){
+//     console.log(`num2 is greater`);
+    
+// }else {
+//     console.log("Error");
+    
+// }
+
+
+// Check a number
+
+// let gpa = 3123
+
+// if(typeof gpa === 'number'){
+//     console.log("A number");
+    
+// } else{
+//     console.log('Not a number');
+    
+// }
+
+// Check Boolean
+
+// let isLogged = true
+
+// if(isLogged){
+//     console.log("True it is");
+// } else{
+//     console.log("Its a false");
+    
+// }
+
+// Check an array = empty or not
+
+let names = ['safeer', 'ali', 'hassan']
+
+if(names == null){
+    console.log("Array is empty");
+}else{
+    console.log("Array is not empty");
+    
+}

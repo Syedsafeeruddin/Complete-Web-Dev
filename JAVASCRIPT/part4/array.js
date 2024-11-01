ae577c3b34ddc166a3a8631ee15edb9795e4be96
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2024-11-01 23:46:37 +0500

    JAVASCRIPT PART 7

---
+++ JAVASCRIPT/part4/array.js
@@ -0,0 +1,68 @@
+// Challenge - 1
+
+// let teaFlavours = ["green tea", "black tea", "oolong tea"]
+// let firstTea = teaFlavours[0]
+// console.log(firstTea);
+
+// Challenge - 2
+
+// let cities = ["London", "Paris", "New York", "Tokyo"]
+// let favouriteCity = cities[2]
+// console.log(favouriteCity);
+
+// Challenge - 3
+
+// let teaTypes = ['herbal tea', 'white tea', 'masala chai']
+// console.log(teaTypes[1] = 'jasmine tea');
+
+// Challenge - 4
+
+// let citiesVisited = ['Mumbai', 'Sydney']
+// console.log("before push");
+// console.log(citiesVisited);
+// let newCity = 'Berlin';
+// citiesVisited.push(newCity)
+// console.log("after push");
+// console.log(citiesVisited);
+
+// Challenge - 5
+
+// let teaOrders = ['chai', 'iced tea', 'matcha', 'earl grey']
+// let lastOrder = teaOrders.pop()
+// console.log(lastOrder);
+
+// Challenge - 6
+
+// let popularTeas = ["green tea", "oolong tea", "chai"]
+// let softCopyTeas = popularTeas
+// console.log(softCopyTeas);
+
+// Challenge - 7
+
+// let topCities = ["Singapore", "Berlin" ,"New York"]
+// let hardCopyCities = [...topCities]
+// topCities.pop()
+// console.log(hardCopyCities);
+
+// Challenge - 8
+
+// let europeanCitites = ["Paris", "Milan"]
+// let asianCities = ["Tokyo", "Karachi"]
+// let worldCities = europeanCitites.concat(asianCities) // array function to join two arrays
+// console.log(worldCities);
+
+// Challenge - 9
+
+// let teaMenu = ["masala chai", "oolong tea", "earl grey"]
+// let menuLength = teaMenu.length
+// console.log(menuLength);
+
+// Challenge - 10
+
+let citybucketlist = ["Tokyo", "London", "Cape Town", "Vancouver"]
+let isLondonInList = citybucketlist.includes("London")
+console.log(isLondonInList);
+
+
+
+

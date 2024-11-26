f795ec705fd886a8a0d060004d2569358214ddde
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2024-11-26 16:52:30 +0500

    Closures

---
+++ JAVASCRIPT/part7_adv/closures.js
@@ -0,0 +1,11 @@
+function outer(){
+    let hello = 1
+    return function inner(){
+        return hello++
+    }
+}
+
+let result = outer() 
+console.log(result());
+console.log(result());
+console.log(result());

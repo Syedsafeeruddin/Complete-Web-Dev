aa5a9756122f040cbd50c068ec46fea052917401
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2024-11-28 22:49:03 +0500

    Functions

---
+++ JAVASCRIPT/part4/functions.js
@@ -0,0 +1,30 @@
+// Challenge - 1
+
+// function makeTea(typeOfTea) {
+//     return console.log(`Making ${typeOfTea}`);
+    
+// }
+
+// let teaOrder = makeTea("Green Tea");
+
+// Challenge - 2
+
+// function orderTea(teaType){
+//     function confirmOrder() {
+//         return `Order confirmed for ${teaType}`
+//     }
+//     return confirmOrder()
+// }
+
+// console.log(orderTea("Safeer"));
+
+// Challenge - 3
+
+const calculatedTotal = (price, quantity) => {
+    cost = price * quantity
+    return `Total is: ${cost}`
+}
+let totalCost = calculatedTotal(20,3)
+console.log(totalCost);
+
+

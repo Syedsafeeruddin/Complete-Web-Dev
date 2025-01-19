b04ac836f5118c5164cb4e5cc1d7479e05a88702
Syed Safeer Uddin <syedsafeeruddi439@gmail.com>
2025-01-19 23:12:11 +0500

    Advance Challenges

--- JAVASCRIPT/part7_adv/AdvanceJsChallenges.js
+++ JAVASCRIPT/part7_adv/AdvanceJsChallenges.js
@@ -152,3 +152,84 @@ const contextTwo = {multiplier : 3}
 
 sum.apply(null, [contextOne, contextTwo])
 
+
+// Challenge - 05
+
+// Task 1
+function fetchUser() {
+  return new Promise(resolve => setTimeout(() => resolve('User data'), 1000));
+}
+ 
+function fetchPosts() {
+  return new Promise(resolve => setTimeout(() => resolve('Posts data'), 1000));
+}
+ 
+async function fetchAllData() {
+  const [user, posts] = await Promise.all([fetchUser(), fetchPosts()]);
+  console.log(user);
+  console.log(posts);
+}
+ 
+// Task 2
+function fetchSuccess() {
+  return new Promise(resolve => setTimeout(() => resolve('Success'), 1000));
+}
+ 
+function fetchFailure() {
+  return new Promise((_, reject) => setTimeout(() => reject('Error occurred'), 1000));
+}
+ 
+async function handlePromises() {
+  try {
+    const [success, failure] = await Promise.all([fetchSuccess(), fetchFailure()]);
+    console.log(success);
+  } catch (error) {
+    console.log(error);
+  }
+}
+ 
+// Task 3
+function fetchWithTimeout(promise, timeout) {
+  const timeoutPromise = new Promise((_, reject) => 
+    setTimeout(() => reject('Timeout exceeded'), timeout)
+  );
+  
+  return Promise.race([promise, timeoutPromise]);
+}
+ 
+function fetchData() {
+  return new Promise(resolve => setTimeout(() => resolve('Data fetched'), 3000));
+}
+
+// Challenge - 06
+
+function* numberGenerator() {
+  yield 1;
+  yield 2;
+  yield 3;
+}
+function rangeIterator(start, end) {
+  let current = start;
+  return {
+    next: function() {
+      if (current <= end) {
+        return { value: current++, done: false };
+      } else {
+        return { value: undefined, done: true };
+      }
+    }
+  };
+}
+
+function* fibonacciGenerator() {
+  let a = 1, b = 1;
+  yield a;
+  yield b;
+  while (true) {
+    let next = a + b;
+    yield next;
+    a = b;
+    b = next;
+  }
+}
+

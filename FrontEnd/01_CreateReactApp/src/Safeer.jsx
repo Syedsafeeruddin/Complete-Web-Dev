ca31aeee1ac541ed36f97d9cb9237c44dd927f54
unknown <syedsafeeruddi439@gmail.com>
2025-06-27 23:28:06 +0500

    React Structure & Flow

---
+++ FrontEnd/01_CreateReactApp/src/Safeer.jsx
@@ -0,0 +1,8 @@
+
+function Safeer(){
+    return(
+        <h2>My name is also Safeer!</h2>
+    ) 
+}
+
+export default Safeer
--- FrontEnd/01_CreateReactApp/src/main.jsx
+++ FrontEnd/01_CreateReactApp/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Safeer from './safeer.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <>
     <App />
-  </StrictMode>,
+    <Safeer />
+    </>
 )

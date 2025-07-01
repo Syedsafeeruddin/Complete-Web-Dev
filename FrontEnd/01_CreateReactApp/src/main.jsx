2605a39bc6c852fc206bfdcaca78688513a2184f
unknown <syedsafeeruddi439@gmail.com>
2025-07-01 14:43:09 +0500

    Learning Basics

--- FrontEnd/01_CreateReactApp/src/main.jsx
+++ FrontEnd/01_CreateReactApp/src/main.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Safeer from './safeer.jsx'
+import Safeer from './Safeer.jsx'
+
+const text = "Safeer"
+
+const reactElement = React.createElement(
+    'h2',
+    {
+        style: {color: 'red'},
+        background: 'black'
+    },
+    'kese ho ',
+    text
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <>
-    <App />
-    <Safeer />
-    </>
+
+    reactElement
+
 )

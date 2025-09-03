f2309d3ec4c532406dc6e466114ce7e270e5a952
unknown <syedsafeeruddi439@gmail.com>
2025-09-04 00:31:50 +0500

    A simple counter App with ShadCN & Acerternity UI

---
+++ LearningProjects/portfolioWeb/counterApp/src/App.jsx
@@ -0,0 +1,81 @@
+import { BackgroundGradient } from "@/components/ui/background-gradient";
+import { TypewriterEffect  } from "@/components/ui/typewriter-effect";
+import { Vortex } from "@/components/ui/vortex";
+// import { Button } from "@/components/ui/button";
+import { HoverBorderGradient } from "@/components/ui/hover-border-gradient";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useState } from "react";
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addCount = () => {
+    setCount(() => count + 1)
+  }
+  const subtractCount = () => {
+    setCount(() => count - 1)
+    if(count == 0){
+      setCount(() => count)
+    }
+  }
+
+const words = [
+  { text: "A\u00A0", className: "text-white text-2xl" },
+  { text: "Simple\u00A0", className: "text-white text-2xl" },
+  { text: "Counter\u00A0", className: "text-white text-2xl" },
+  { text: "App.", className: "text-blue-500 text-2xl" },
+];
+
+
+  return (
+    <div className="bg-black flex min-h-svh flex-col items-center justify-center">
+      <Vortex
+        backgroundColor="black"
+        rangeY={800}
+        particleCount={500}
+        baseHue={120}
+        className="flex items-center flex-col justify-center px-2 md:px-10  py-4 w-full h-full"
+      >
+      <BackgroundGradient>
+      <Card className='w-sm bg-gray-700 border-black'>
+        <CardHeader>
+          <CardTitle className='flex items-center justify-center text-5xl text-gray-300'>Counter App</CardTitle>
+          <CardDescription className='flex items-center justify-center text-2xl text-gray-300'>
+             <TypewriterEffect words={words} />
+          </CardDescription>
+        </CardHeader>
+        <CardContent className='flex items-center justify-center text-5xl text-gray-300'>
+          <p>{count}</p>
+        </CardContent>
+        <CardFooter className='flex items-center justify-center gap-2'>
+          <HoverBorderGradient>
+          <button
+          onClick={addCount}
+          borderRadius="1.75rem"
+          > Count + 1
+          </button>
+          </HoverBorderGradient>
+          <HoverBorderGradient>
+          <button
+          onClick={subtractCount}
+          borderRadius="1.75rem"
+          > Count - 1
+          </button>
+          </HoverBorderGradient>
+        </CardFooter>
+      </Card>
+      </BackgroundGradient>
+      </Vortex>
+    </div>
+  );
+}
+
+export default App;
